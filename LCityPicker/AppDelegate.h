b30fdef0968be7aa843a637c7eca8838a6702db3
Args <ldjdjdj@163.com>
2016-04-28 13:45:51 +0800

    Initial Commit

---
+++ LCityPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCityPicker
+//
+//  Created by tih on 16/4/28.
+//  Copyright © 2016年 TOSHIBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCityPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCityPicker
+//
+//  Created by tih on 16/4/28.
+//  Copyright © 2016年 TOSHIBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
